f4726e7f65116aed0ca606742ded273286f80e33
Adrian Calavie <adrian.calavie06@gmail.com>
2026-03-02 18:26:29 +0200

    Construct Puzzle from matrix and validate rank

--- src/puzzle.rs
+++ src/puzzle.rs
@@ -1,4 +1,4 @@
-use anyhow::{Result, bail};
+use anyhow::{Context, Ok, Result, bail};
 use num::{Num, ToPrimitive};
 use std::{collections::HashSet, str::FromStr};
 
@@ -9,12 +9,18 @@ pub struct Puzzle {
 }
 
 impl Puzzle {
-    pub fn new(state: PuzzleState, rank: u8) -> Self {
-        Self {
+    pub fn new(matrix: Vec<Vec<u32>>, rank: u8) -> Result<Self> {
+        let state = PuzzleState::new(matrix)?;
+
+        if !rank_matches_matrix(&state.matrix, rank)? {
+            bail!("Rank does not match matrix shape.");
+        }
+
+        Ok(Self {
             state,
             previous_states: Vec::new(),
             rank,
-        }
+        })
     }
 }
 
@@ -114,6 +120,22 @@ where
     }
 
     seen.len() == total
+}
+
+fn rank_matches_matrix<T>(matrix: &[Vec<T>], rank: u8) -> Result<bool>
+where
+    T: Num,
+{
+    let row_count = matrix.len();
+    let col_count = matrix.first().unwrap().len();
+
+    let rank_usize = rank.to_usize().with_context(|| "Rank too high")?;
+
+    if row_count != rank_usize || col_count != rank_usize {
+        return Ok(false);
+    }
+
+    Ok(true)
 }
 
 #[cfg(test)]
